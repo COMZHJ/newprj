1d6f793bc2353f2b4a4426b97543f342bb317541
COMZHJ <COMZHJ@163.com>
2019-06-27 11:16:58 +0800

    测试

---
+++ day2/deme2.py
@@ -0,0 +1,15 @@
+import numpy as np
+from PIL import Image
+
+im = Image.open('../pic/phone.jpg')
+
+print(im, type(im))
+im = np.array(im)
+
+print(im, type(im), im.shape, im.dtype)
+im = [255, 255, 255] - im
+print(im, type(im), im.shape, im.dtype)
+
+im = Image.fromarray(im.astype(np.uint8))
+im.save('../pic/phone2.jpg')
+
--- day2/demo1.py
+++ day2/demo1.py
@@ -5,5 +5,7 @@ t1[0, 0] = 100
 print(t1, t1.shape)
 print(t1[0:2, 1:])
 print(t1[0:2, [1, 3]])
+print(t1[[0, 2], [1, 3]])
 
-
+t2 = t1[:, [1, 3]]
+print(t2, t2.shape)
